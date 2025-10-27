43a4a9cf020029d7ae84a5cb7ebfd2326e6de6ae
MartijnHoek <martynhoek@gmail.com>
2025-10-27 14:44:49 +0100

    added Kia BD database entry

---
+++ WebPage/database/kia/models/forte-bd.php
@@ -0,0 +1,28 @@
+<?php
+include_once __DIR__ . '/../../_shared/helpers.php';
+
+return [
+    'name' => 'Kia Forte (BD)',
+    'production-years' => '2018-2024',
+    'description' => 'The Kia Forte (BD) is the third generation of the Kia Forte compact car, produced by the South Korean automaker Kia from 2018 to 2024. <br><br>
+                      This generation of the Forte continued to build on its reputation for stylish design, fuel efficiency, and affordability. <br><br>
+                      The Forte (BD) featured a more modern and aggressive design, advanced safety features, updated infotainment systems, and improved interior comfort. <br><br>
+                      It was available in various trims and engine options, catering to a wide range of customers looking for a reliable and practical vehicle in the compact car segment.',
+    'image' => '',
+    'rebagde' => [
+        'Kia Cerato <span class="fi fi-kr flag-medium"></span>',
+        'Kia K3 <span class="fi fi-kr flag-medium"></span>'
+    ],
+    'country-origin-flag' => '<span class="fi fi-kr flag-medium"></span>',
+    // todo add engines
+    'engines' => [
+    ],
+    'assembly-plants' => [
+        plantLabel('hwaseong'),
+        plantLabel('pesqueria'),
+        plantLabel('yancheng'),
+        plantLabel('avtotor'),
+        plantLabel('gloviz'),
+        plantLabel('chu-lai'),
+        ]
+];
